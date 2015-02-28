5127d98afee43b29692541db2b9b9c01d9b09b64
Minh Luu <mluu510@gmail.com>
2015-02-28 01:09:28 -0800

    Initial Commit

---
+++ Chat Bubbles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat Bubbles
+//
+//  Created by Minh Luu on 2/28/15.
+//  Copyright (c) 2015 Minh Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Chat Bubbles/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Chat Bubbles
+//
+//  Created by Minh Luu on 2/28/15.
+//  Copyright (c) 2015 Minh Luu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
